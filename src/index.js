b5aa3da893b34f36460891f88750baf2ea2fa173
Oran <oranyee@163.com>
2024-11-25 17:54:31 +0800

    return help doc

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import DOCS from './help.html'
+
 addEventListener("fetch", (event) => {
   event.passThroughOnException();
   event.respondWith(handleRequest(event.request));
@@ -42,6 +44,15 @@ async function handleRequest(request) {
         status: 404,
       }
     );
+  }
+// return docs
+  if (url.pathname === "/") {
+    return new Response(DOCS, {
+      status: 200,
+      headers: {
+        "content-type": "text/html"
+      }
+    });
   }
   const isDockerHub = upstream == dockerHub;
   const authorization = request.headers.get("Authorization");
@@ -159,17 +170,5 @@ function responseUnauthorized(url) {
   return new Response(JSON.stringify({ message: "UNAUTHORIZED" }), {
     status: 401,
     headers: headers,
-  });
-}
-
-import DOCS from './help.html'
- 
-// return docs
-if (url.pathname === "/") {
-  return new Response(DOCS, {
-    status: 200,
-    headers: {
-      "content-type": "text/html"
-    }
   });
 }
